bddd76b93dfd8f3eb441621f4f8a7c8bf4e16ecb
Adamu Fura <adamufura98@gmail.com>
2026-07-08 21:37:04 +0100

    feat: initial Spring Boot setup

---
+++ src/main/java/com/bowmenn/bowmenn_api/BowmennApiApplication.java
@@ -0,0 +1,13 @@
+package com.bowmenn.bowmenn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowmennApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowmennApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bowmenn/bowmenn_api/BowmennApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowmenn.bowmenn_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowmennApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
